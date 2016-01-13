74ef93105035d1b1ef0a14c22d6ac7dbcae44d05
ranyan <ranyan@gmail.com>
2016-01-13 18:15:42 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ src/model/City.java
@@ -0,0 +1,32 @@
+package model;
+
+public class City {
+	private int id;
+	private String cityName;
+	private String cityCode;
+	private int provinceId;
+	public int getId() {
+	return id;
+	}
+	public void setId(int id) {
+	this.id = id;
+	}
+	public String getCityName() {
+	return cityName;
+	}
+	public void setCityName(String cityName) {
+	this.cityName = cityName;
+	}
+	public void setCityCode(String code){
+		cityCode = code;
+	}
+	public String getCityCode() {
+	return cityCode;
+	}
+	public int getProvinceId(){
+		return provinceId;
+	}
+	public void setProvinceId(int id){
+		provinceId = id;
+	}
+}
